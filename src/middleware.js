c04726ec9d498c9326ea4e4dedded3ce303d0f57
deffinad <deffin@armsolusi.com>
2025-03-08 03:06:23 +0700

    fix: middleware

--- src/middleware.js
+++ src/middleware.js
@@ -9,15 +9,16 @@ export function middleware(request) {
     const isMobile = /Mobile|Android|iPhone|iPad|iPod/i.test(userAgent);
     const isTablet = /Tablet|iPad/i.test(userAgent);
 
+    // If device is mobile or tablet, redirect to "/mobile"
+    if (isMobile || isTablet) {
+        return NextResponse.redirect(new URL("/mobile", request.url));
+    }
+    
     // If profile does not exist, redirect to homepage
     if (!profile) {
         return NextResponse.redirect(new URL("/", request.url));
     }
 
-    // If device is mobile or tablet, redirect to "/mobile"
-    if (isMobile || isTablet) {
-        return NextResponse.redirect(new URL("/mobile", request.url));
-    }
 
     // Otherwise, continue with the request
     return NextResponse.next();
